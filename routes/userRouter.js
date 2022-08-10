13e8960acbdb33b1e4eb7a892a4fae1a2337de15
saharabanu <saharabanungn@gmail.com>
2022-08-10 10:11:42 +0600

    bcrypt added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,36 +5,48 @@ const bcrypt = require("bcrypt");
 // post user api
 router.post("/create", async (req, res) => {
   let { password, name, email } = req.body;
-  // let pass = password;
-  password = bcrypt.hash(password, 10, function (err, hash) {
-    console.log(password, "hello", hash);
-  });
+  const salt = await bcrypt.genSalt(10);
 
-  // const user = await new User(req.body);
-  // const data = await user.save();
-  // if (data) {
-  //   res.status(201).json({ message: "User created Successfully" });
-  // } else {
-  //   res.status(501).json({ message: "User not created, Something went wrong" });
-  // }
+  password = await bcrypt.hash(password, salt);
+  // console.log(password);
+
+  const user = await new User({ name, email, password });
+  const data = await user.save();
+  if (data) {
+    res.status(201).json({ message: "User created Successfully" });
+  } else {
+    res.status(501).json({ message: "User not created, Something went wrong" });
+  }
 });
 // get user api
 router.get("", async (req, res) => {
-  const data = await User.find().select({
-    _id: 0,
-    password: 0,
-  });
+  const data = await User.find();
+  // const data = await User.find().select({
+  //   _id: 0,
+  //   password: 0,
+  // });
+
   if (data) {
     res.status(200).json({ message: "User ", data });
   } else {
     res.status(404).json({ message: "User not Found, Something went wrong" });
   }
-  console.log(data);
+  // console.log(data);
 });
 
 // login by email api
 router.post("/login", async (req, res) => {
+  let { name, email, password } = req.body;
   const user = await User.find({ email: req.body.email });
+  // console.log(user[0].name);
+
+  bcrypt.compare(
+    password,
+    user[0]?.password ? user[0]?.password : "need right password",
+    function (err, result) {
+      console.log(result);
+    }
+  );
   if (user) {
     res.status(200).json({ message: "User Found", user });
   } else {
